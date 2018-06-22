88c015d618741b7316b323297c81cd9252a82836
Jesse Butryn <jesse.butryn@joyent.com>
2018-06-22 15:15:44 -0600

    Added newMessage and createMessage events

--- server/server.js
+++ server/server.js
@@ -14,6 +14,16 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.emit('newMessage', {
+		from: 'Server',
+		text: 'test from server',
+		createdAt: 1231
+	});
+
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+	});
+
 	socket.on('disconnect', () => {
 		console.log('User was disconnected');
 	});
