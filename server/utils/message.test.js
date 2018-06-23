df3695002c919b99980c038c133cacc8f24d30c3
Jesse Butryn <jesse.butryn@joyent.com>
2018-06-23 13:32:55 -0600

    Added geolocation support and location test

--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,15 +1,29 @@
 var expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
 	it('should generate the correct message object', () => {
-		var from = 'Test';
+		var from = 'testUser';
 		var text = 'A message';
 		var message = generateMessage(from, text);
 		
 		expect(message.createdAt).toBeA('number');
 		expect(message).toInclude({from, text});
+
+	});
+});
+
+describe('generateLocationMessage', () => {
+	it('should generate the correct location object', () => {
+		var from = 'testUser';
+		var latitude = 100;
+		var longitude = -100;
+		var url = 'https://www.google.com/maps?q=100,-100'
+		var locationMessage = generateLocationMessage(from, latitude, longitude);
+
+		expect(locationMessage.createdAt).toBeA('number');
+		expect(locationMessage).toInclude({from, url});
 
 	});
 });
